349499a5c3a2913a84ca999917cfc3980fc42b5c
dakz60 <daichishougu8@gmail.com>
2026-04-20 10:26:36 +0900

    Update boardPosts.js

--- data/boardPosts.js
+++ data/boardPosts.js
@@ -6,5 +6,13 @@ const boardPosts = [
   method: ["会場手渡し", "都内手渡し"],
   oshi: ["谷口愛季", "山下瞳月"],
   twitterUrl: "https://x.com/dazhi2s_SSS/status/2045584302969409745?s=20"
+},
+{
+  give: ["p019","p031""],
+  want: ["p015","p054"],
+  place: ["京都リアミ","ツアー香川"],
+  method: ["会場手渡し", "郵送"],
+  oshi: ["遠藤理子"],
+  twitterUrl: "https://x.com/koara0525/status/2045673213485797767?s=20"
 }
 ];
